64d1b661b51baeec1aff5c716c7247eed106a7bd
unknown <uday.devikar@gmail.com>
2024-10-02 18:43:01 +0530

    formatted code

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4')
-const {default: axios} = require('axios')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { default: axios } = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 async function startServer() {
-    const app = express()
-    const server = new ApolloServer({
-        typeDefs: `
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
 
             type User {
                 id: ID
@@ -25,37 +24,38 @@ async function startServer() {
                 completed: Boolean
                 user: User
             }
-
-
             type Query {
                 getTodos : [Todo]
                 getAllUsers: [User]
                 getUser(id: ID): User
             }
-        
         `,
-        resolvers: {
-            Todo : {
-                user : async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)).data
-            },
+    resolvers: {
+      Todo: {
+        user: async (todo) =>
+          (
+            await axios.get(
+              `https://jsonplaceholder.typicode.com/users/${todo.userId}`
+            )
+          ).data,
+      },
+      Query: {
+        getTodos: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+        getAllUsers: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+        getUser: async (parent, { id }) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+            .data,
+      },
+    },
+  });
 
-            Query : {
-                getTodos: async () =>  (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
-                getAllUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
-                getUser: async (parent, { id }) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data
-            }
-        }
-    })
-
-    app.use(bodyParser.json())
-    app.use(cors())
-
-    await server.start()
-
-    app.use('/graphql', expressMiddleware(server))
-
-    app.listen(8000, () => console.log('server started at port 8000'))
+  app.use(bodyParser.json());
+  app.use(cors());
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(8000, () => console.log("server started at port 8000"));
 }
 
-
-startServer()
+startServer();
